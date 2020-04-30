2bbccdb38dfea2323c65f1b29c6e05dafff8faae
adeoke <adesmail100@gmail.com>
2020-04-30 22:42:44 +0100

    empty list test added

--- app/thumbnail_resizer.py
+++ app/thumbnail_resizer.py
@@ -3,14 +3,18 @@ This is a redone version of the thumbnail maker lib from Tim Ojo.
 The reason for this is that it didn't work for me and rather than fix his
 implementation I decided to completely re-write it for learning purposes.
 """
+import os
 
 
 class Downloader:
-    def __init__(self):
-        pass
+    def __init__(self, home_dir='.'):
+        self.home_dir = home_dir
+        self.input_dir = self.home_dir + os.path.sep + 'incoming'
+        self.output_dir = self.home_dir + os.path.sep + 'outgoing'
 
     def download_files(self, li):
-        pass
+        if not li:
+            return 'list is empty'
 
     def delete_files(self, li):
         pass
--- tests/unit/test_thumbnail_resizer.py
+++ tests/unit/test_thumbnail_resizer.py
@@ -4,12 +4,15 @@ from app.thumbnail_resizer import Downloader
 
 
 class TestDownloader(unittest.TestCase):
+    EXPECTED_EMPTY_LIST_MESSAGE = 'list is empty'
+
     def setUp(self):
         self.downloader = Downloader()
 
-    def test_downloading(self):
+    def test_list_empty_message_returned(self):
         li = []
-        self.downloader.download_files(li)
+        actual_result = self.downloader.download_files(li)
+        self.assertEqual(actual_result, self.EXPECTED_EMPTY_LIST_MESSAGE)
 
 
 if __name__ == '__main__':
